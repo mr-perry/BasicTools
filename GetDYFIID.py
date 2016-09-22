2f76d639f294870c0db531d15c84ad74abffdaa1
Perry <mrperry@igskcicgltgm009.cr.usgs.gov>
2016-09-22 15:25:21 -0600

    GetDYFI added

--- GetDYFIID.py
+++ GetDYFIID.py
@@ -14,6 +14,8 @@ def main():
 	if os.path.isfile(fname):
 		BASE = 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventid=%s'
 		DYFIID = [];
+		EventID = [];
+		Skip = [];
 		with open(fname) as f:
 			content = f.readlines()
 		for ii in range(len(content)):
@@ -23,11 +25,19 @@ def main():
 			data = fh.read().decode('utf-8')
 			fh.close()
 			jdict = json.loads(data)
-			DYFIID.append(jdict['properties']['products']['dyfi'][0]['code'])
+			try:
+				DYFIID.append(jdict['properties']['products']['dyfi'][0]['code'])
+				EventID.append(content[ii])
+			except:
+				Skip.append(content[ii])
 		with open('DYFI.id','wb') as csvfile:
 			spamwriter = csv.writer(csvfile,delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
 			for ii in range(len(DYFIID)):
-				spamwriter.writerow((content[ii],DYFIID[ii]))
+				spamwriter.writerow((EventID[ii],DYFIID[ii]))
+		with open('NoDYFI.id','wb') as csvfile:
+			spamwriter = csv.writer(csvfile,delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			for ii in range(len(Skip)):
+				spamwriter.writerow(Skip[ii])
 	else:
 		print('File Not Found')
 
